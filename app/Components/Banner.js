c5fbed4d6f034edae8704485c866f8323e1f6fa4
j-shifat-18 <info.jahirulsifat@gmail.com>
2025-08-22 19:49:23 +0600

    update banner title and description

--- app/Components/Banner.js
+++ app/Components/Banner.js
@@ -12,13 +12,18 @@ const Banner = () => {
       <div className="hero-overlay"></div>
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-5xl font-bold">Find Your Dream Car</h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Explore a wide range of cars, from sleek sports models to reliable
+            family vehicles. Browse, compare, and find the perfect car for your
+            lifestyle today.
           </p>
-          <Link href="browse-cars" className="btn shadow-none border-none text-white bg-red-500 hover:bg-red-600">Get Started</Link>
+          <Link
+            href="browse-cars"
+            className="btn shadow-none border-none text-white bg-red-500 hover:bg-red-600"
+          >
+            Get Started
+          </Link>
         </div>
       </div>
     </div>
